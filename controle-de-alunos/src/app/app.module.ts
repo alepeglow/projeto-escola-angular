d2770b91b550f87a80083a039e2459ede74c4c6b
Alessandra Peglow <peglowalessandra@gmail.com>
2025-06-27 19:54:09 -0300

    Gerando componente aluno

--- controle-de-alunos/src/app/app.module.ts
+++ controle-de-alunos/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlunoComponent } from './componentes/aluno/aluno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunoComponent
   ],
   imports: [
     BrowserModule,
---
+++ controle-de-alunos/src/app/componentes/aluno/aluno.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aluno',
+  standalone: false,
+  templateUrl: './aluno.component.html',
+  styleUrl: './aluno.component.css'
+})
+export class AlunoComponent {
+
+}
